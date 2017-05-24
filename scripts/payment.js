2eb7f35219259b8e0732fe57b8f29e506c8b47d7
Yuri Aksenov <aksenov.mailbox@gmail.com>
2017-05-24 17:56:34 +0300

    внесены правки в стили

--- scripts/payment.js
+++ scripts/payment.js
@@ -5,11 +5,24 @@ $(".payment-list").click(function() {
     $(".payment-list").text("Скрыть все методы оплаты");
     $(".pkp-payment-method").css("backgroundColor", "#f7f5f3");
     $(".pkp-payment-method").css("marginTop", "16px");
-    $(".pkp-payment-method").css("paddingTop", "10px");
+    $(".pkp-payment-method").css("marginBottom", "27px");
+    $(".pkp-payment-method").css("paddingTop", "12px");
+    $(".payment-list--bg").show();
   } else {
     $(".payment-list").text("Показать все методы оплаты");
     $(".pkp-payment-method").css("backgroundColor", "#fff");
     $(".pkp-payment-method").css("marginTop", 0);
-    $(".pkp-payment-method").css("paddingTop", "27px");
+    $(".pkp-payment-method").css("marginBottom", "17px");
+    $(".pkp-payment-method").css("paddingTop", "8px");
+    $(".payment-list--bg").hide();
   }
+});
+
+$(".pkp-payment-method__search-button").click(function() {
+  $(".pkp-payment-method__search-form").toggleClass("pkp-payment-method__search-form--show");
+});
+
+$(".pkp-payment-method__close-button").click(function() {
+  $(".pkp-payment-method__search-form").toggleClass("pkp-payment-method__search-form--show");
+  $(".pkp-payment-method__search-field").val("");
 });
